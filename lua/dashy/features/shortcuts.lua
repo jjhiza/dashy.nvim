f64d0359b9c509bf87716269f8bd6a1bc6983d7d
Jay Hiza <mini_jay@Jays-Mac-mini.local>
2025-04-21 16:03:33 -0400

    attempt 8

--- lua/dashy/features/shortcuts.lua
+++ lua/dashy/features/shortcuts.lua
@@ -51,7 +51,7 @@ M.config = {
     {
       icon = "󰊄",
       label = "Find Word",
-      key = "g",
+      key = "w",
       action = function()
         if pcall(require, "telescope.builtin") then
           require("telescope.builtin").live_grep()
